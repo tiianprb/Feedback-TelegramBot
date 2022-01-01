205206cddd7918afd3ca17b5abf126d48e54b37e
tiianprb <96933129+tiianprb@users.noreply.github.com>
2022-01-01 09:25:07 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TG_TOKEN = "1625666269:AAFo3l_wF2Lclg-4DGfDZUVprTBGZx-OigI"
+TG_TOKEN = "5050150479:AAErYdKzK0WeSoV3tJ1nUbVcSYR4dU8vGQU"
 PROXY = None
-CHAT_ID = -1001445044781 # The chat group or individual chat id of forwarded message destination
+CHAT_ID = 1892879990 # The chat group or individual chat id of forwarded message destination
 BOT_TIMEOUT = 10
 BOT_RETRIES = 5
 USER_JSON = 'user_book.json'
